d7da79ad5a6c3433871a284fbcfb7bbb4c4ebfaa
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-04-24 00:00:47 -0400

    Add new user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  resources :users
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
