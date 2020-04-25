d9dfc7cf32676cc9eac3170bd20101a662e656ad
Raul Jiminian <raul.e.jiminian@gmail.com>
2020-04-25 18:33:54 -0400

    Add authenticate conditionals

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,19 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  # def require_user
+  #   if !logged_in?
+  #     flash[:danger] = "You must be logged in to perform that action"
+  #     redirect_to root_path
+  #   end
+  # end
+
 end
